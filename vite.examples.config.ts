a3caf9966ca69d203afbab4caec3b00fad8a512c
jonasholmer <jonas@mindmore.com>
2025-04-01 12:13:52 +0200

    GH Pages

---
+++ vite.examples.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from 'node:path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  base: "./",
+  root: resolve(__dirname, 'examples'),
+  build: {
+    target: "es2022",
+    outDir: "dist",
+    rollupOptions: {
+      input: resolve(__dirname, 'examples/index.html'),
+    },
+  },
+  plugins: [
+    tsconfigPaths(),
+  ],
+});
