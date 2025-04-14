2c5f558c3f36da103f86198a0c63a1c7d8462303
jonasholmer <jonas@mindmore.com>
2025-04-14 22:01:54 +0200

    Split dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from "vite";
 import { dirname, resolve } from 'node:path'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath } from 'node:url'
+import { visualizer } from "rollup-plugin-visualizer";
 
 function crossOriginIsolationMiddleware(_: any, response: any, next: any) {
   response.setHeader("Cross-Origin-Opener-Policy", "same-origin");
@@ -20,10 +21,14 @@ export default defineConfig({
     emptyOutDir: true,
     emitAssets: true,
     assetsInlineLimit: 0,
+    minify: "esbuild",
     rollupOptions: {
-      preserveEntrySignatures: 'exports-only',
+      treeshake: 'smallest',
+      preserveEntrySignatures: 'strict',
       input: resolve(__dirname, 'src/index.ts'),
       output: {
+        compact: true,
+        manualChunks: { xstate: ['xstate'], 'sqlocal': ['sqlocal']},
         format: 'es',
         entryFileNames: 'workhorse.js',
         chunkFileNames: 'chunks/[name]-[hash].js',
@@ -38,6 +43,7 @@ export default defineConfig({
       configureServer: server => { server.middlewares.use(crossOriginIsolationMiddleware); },
       configurePreviewServer: server => { server.middlewares.use(crossOriginIsolationMiddleware); },
     },
+    visualizer(),
   ],
   optimizeDeps: {
     exclude: ["sqlocal"],
