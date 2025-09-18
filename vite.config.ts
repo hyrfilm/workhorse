0eea4fb474c09fa47c4aaff0353b503aadd4d8d2
jonasholmer <jonas@mindmore.com>
2025-09-18 02:05:00 +0200

    Add CommonJS compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,64 @@
 import {defineConfig} from "vite";
-import { dirname, resolve } from 'node:path'
+import {dirname, resolve} from 'node:path'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { fileURLToPath } from 'node:url'
+import {fileURLToPath} from 'node:url'
 
 function crossOriginIsolationMiddleware(_: any, response: any, next: any) {
-  response.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  response.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-  next();
+    response.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    response.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+    next();
 }
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  base: "./",
-  build: {
-    target: "es2022",
-    sourcemap: true,
-    outDir: "dist",
-    emptyOutDir: true,
-    emitAssets: true,
-    assetsInlineLimit: 0,
-    rollupOptions: {
-      preserveEntrySignatures: 'exports-only',
-      input: resolve(__dirname, 'src/index.ts'),
-      output: {
-        format: 'es',
-        entryFileNames: 'workhorse.js',
-        chunkFileNames: 'chunks/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash][extname]',
-      },
+    base: "./",
+    build: {
+        target: "es2022",
+        sourcemap: true,
+        outDir: "dist",
+        emptyOutDir: true,
+        emitAssets: true,
+        assetsInlineLimit: 0,
+        rollupOptions: {
+            preserveEntrySignatures: 'exports-only',
+            input: resolve(__dirname, 'src/index.ts'),
+            output: [
+                // ESM
+                {
+                    format: 'es',
+                    entryFileNames: 'workhorse.js',
+                    chunkFileNames: 'chunks/[name]-[hash].js',
+                    assetFileNames: 'assets/[name]-[hash][extname]',
+                },
+                // CommonJS for compatibility
+                {
+                    format: 'cjs',
+                    entryFileNames: 'workhorse.cjs',
+                    inlineDynamicImports: true,
+                    assetFileNames: 'assets/[name]-[hash][extname]',
+                },
+            ],
+        },
     },
-  },
-  plugins: [
-    tsconfigPaths(),
-    {
-      name: 'cross-origin-isolation',
-      configureServer: server => { server.middlewares.use(crossOriginIsolationMiddleware); },
-      configurePreviewServer: server => { server.middlewares.use(crossOriginIsolationMiddleware); },
+    plugins: [
+        tsconfigPaths(),
+        {
+            name: 'cross-origin-isolation',
+            configureServer: server => {
+                server.middlewares.use(crossOriginIsolationMiddleware);
+            },
+            configurePreviewServer: server => {
+                server.middlewares.use(crossOriginIsolationMiddleware);
+            },
+        },
+    ],
+    optimizeDeps: {
+        exclude: ["sqlocal"],
     },
-  ],
-  optimizeDeps: {
-    exclude: ["sqlocal"],
-  },
-  server: {
-    hmr: {
-      host: 'localhost',
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
     },
-  },
 });
