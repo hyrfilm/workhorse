d62d165430c84c2d71a08b044ea3be59d7664383
Jonas Holmer <jonas.holmer@gmail.com>
2026-08-29 11:22:08 +0200

    Config tweaks (#16)

--- vite.config.ts
+++ vite.config.ts
@@ -45,6 +45,9 @@ export default defineConfig({
     optimizeDeps: {
         exclude: ["sqlocal"],
     },
+    worker: {
+        format: 'es',
+    },
     server: {
         hmr: {
             host: 'localhost',
